43aa6d533fe2246cdb8a51ceb547690d79ce9572
Ihar <50808516+IharNikalayeu@users.noreply.github.com>
2022-08-21 00:21:32 +0300

    Revert "learning functions"

--- functions/task_3.js
+++
@@ -1,9 +0,0 @@
-// 3. Дан массив [{name:'Bogdan', age: '20'}, {name:'Vlad', age: '20'}]. Получить массив вида ['Name: Bogdan, Age: 20', ...]. Получение строк вынести в функцию
-
-const users = [
-  { name: "Bogdan", age: "20" },
-  { name: "Vlad", age: "20" },
-];
-const getUserToString = ({ name, age }) => `Name: ${name}, Age: ${age}`;
-const usersToString = users.map(getUserToString);
-console.log(usersToString);
